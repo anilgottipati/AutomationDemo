1c9216be84afc225a91cf044711303338174a0e0
Hareesh <harishhari79894@gmail.com>
2024-11-29 20:15:22 +0530

    updated chrome location path

--- src/test/java/launchBrowser.java
+++ src/test/java/launchBrowser.java
@@ -5,16 +5,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class launchBrowser {
+    private static String[] args;
+
     public static void main(String[] args) throws InterruptedException {
+        launchBrowser.args = args;
         // Setup WebDriver (automatically manages the browser driver)
         WebDriverManager.chromedriver().setup();
 
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Anil\\Desktop\\GITDemo\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Hareesh\\Desktop\\chrome\\chromedriver.exe");
         // Create an instance of ChromeDriver
         WebDriver driver = new ChromeDriver();
         // Open a website
         driver.get("https://www.google.com");
-        Thread.sleep(1000);
+        Thread.sleep(50000);
         // Print the title of the page
         System.out.println("Page Title: " + driver.getTitle());
         String Actual = driver.getTitle();
