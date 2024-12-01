1d0b2c93e0ba8b7dea35844c333028f6b68c2658
Surekha <rachurisurekha2002@gmail.com>
2024-12-01 17:31:22 +0530

    Created Sample Scripts

---
+++ src/test/java/Demotest.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demotest {
+    public static void main (string[] args)  {
+    WebDriver driver = new ChromeDriver();
+    driver.get("http://demoqa.com");
+
+
+
+}
+
+
+}
+
+
