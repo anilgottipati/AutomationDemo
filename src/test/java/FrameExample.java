de6a5273a9b34d05ada8901ca69f22e429e84feb
Anil <anil.gottipati@transcore.com>
2024-12-02 22:44:06 +0530

    Created Frame Script

---
+++ src/test/java/FrameExample.java
@@ -0,0 +1,24 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import junit.framework.Assert;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FrameExample {
+    public static void main(String[] args) throws InterruptedException {
+        // Setup WebDriver (automatically manages the browser driver)
+        WebDriverManager.chromedriver().setup();
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Anil\\Desktop\\GITDemo\\chromedriver.exe");
+        // Create an instance of ChromeDriver
+        WebDriver driver = new ChromeDriver();
+        // Open a website
+        driver.get("https://demoqa.com/frames");
+        Thread.sleep(10000);
+        driver.switchTo().frame("frame1");
+        String ele = driver.findElement(By.id("sampleHeading")).getText();
+        // Close the browser
+        driver.close();
+    }
+}
