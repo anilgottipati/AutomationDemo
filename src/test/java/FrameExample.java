9d2d2f5f01ecf651da8e1e98d06556eb9bf24ac9
anilgottipati <95089047+anilgottipati@users.noreply.github.com>
2024-12-06 18:49:25 +0530

    Update FrameExample.java

--- src/test/java/FrameExample.java
+++ src/test/java/FrameExample.java
@@ -11,7 +11,7 @@ public class FrameExample {
         WebDriverManager.chromedriver().setup();
 
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Anil\\Desktop\\GITDemo\\chromedriver.exe");
-        // Create an instance of ChromeDriver
+        // Create an instance of Anil
         WebDriver driver = new ChromeDriver();
         // Open a website
         driver.get("https://demoqa.com/frames");
