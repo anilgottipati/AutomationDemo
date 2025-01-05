fb75e7beccf0bce3375106e8fd651270b3eba79f
Anil G <gottipatianilkumar@gmail.com>
2025-01-05 15:01:42 +0530

    Java

--- src/test/java/SeleniumCommands/IsEnabledExample1.java
+++ src/test/java/SeleniumCommands/IsEnabledExample1.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class IsEnabledExample {
+public class IsEnabledExample1 {
     public static void main(String[] args) throws InterruptedException {
         // Setup WebDriver (automatically manages the browser driver)
         WebDriverManager.chromedriver().setup();
