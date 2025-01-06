cf44cf6cad3ff8572840a3689b82357f6e22ff5c
Anil G <gottipatianilkumar@gmail.com>
2025-01-06 08:17:50 +0530

    Java

---
+++ src/test/java/SeleniumCommands/JavaScriptExecutorExample.java
@@ -0,0 +1,29 @@
+package SeleniumCommands;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import junit.framework.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JavaScriptExecutorExample {
+    public static void main(String[] args) throws InterruptedException {
+        // Setup WebDriver (automatically manages the browser driver)
+        WebDriverManager.chromedriver().setup();
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Anil G\\Desktop\\Anil\\chromedriver.exe");
+        // Create an instance of ChromeDriver
+        WebDriver driver = new ChromeDriver();
+        // Open a website
+        driver.get("file:///C:/Users/Anil%20G/Desktop/Anil/SeleniumWebSiteTraining/IsEnabled.html");
+        Thread.sleep(5000);
+        boolean ele = driver.findElement(By.id("disabledBtn")).isEnabled();
+        Assert.assertFalse(ele);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("document.getElementById('email').value='SoftwareTestingMaterial.com';");
+
+        // Close the browser
+        driver.close();
+    }
+}
