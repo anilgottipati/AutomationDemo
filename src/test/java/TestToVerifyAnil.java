983ea5c043a24b1fe3d28d3f915a668c803d87de
Anil G <gottipatianilkumar@gmail.com>
2025-02-17 15:36:55 +0530

    Git Unix File

--- src/test/java/TestToVerifyAnil.java
+++ src/test/java/TestToVerifyAnil.java
@@ -4,18 +4,24 @@ import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class TestToVerifyAnil {
     public static void main(String[] args) throws InterruptedException {
         // Setup WebDriver (automatically manages the browser driver)
         WebDriverManager.chromedriver().setup();
 
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Anil\\Desktop\\GITDemo\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Anil G\\Desktop\\Anil\\chromedriver.exe");
         // Create an instance of ChromeDriver
         WebDriver driver = new ChromeDriver();
         // Open a website
-        driver.get("https://demoqa.com/");
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        String a = "Anil";
 
+        Select Se = new Select(driver.findElement(By.id("dropdown-class-example")));
+        Se.selectByValue("option2");
+        Se.selectByIndex(3);
+        Se.selectByVisibleText("Option1");
         // Close the browser
         driver.close();
     }
